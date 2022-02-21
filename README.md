78ccb5f9f238bc1c3ecf189605a73876a8a2e26d
Quintana Ainhoa <quintana.131697@e.unavarra.es>
2022-02-21 08:20:44 +0100

    fizzbuzz anadido

---
+++ README.md
@@ -0,0 +1,7 @@
+All projects go here
+<<<<<<< HEAD
+=======
+Use git submodules:
+- https://git-scm.com/book/en/v2/Git-Tools-Submodules
+- git submodule add https://github.com/.../...
+>>>>>>> ef35902ffd11409eee1d05ca1619ff3d0cb8d4fe
---
+++ primeFactors/src/primeFactors.php
@@ -0,0 +1,18 @@
+<?php
+
+class primeFactors
+{
+    public function generate ($number){
+        $factors = [];
+        $divisor = 2;
+        while($number>1) {
+            while ($number % $divisor == 0) {
+                $factors = $divisor;
+                $number = $number / $divisor;
+            }
+            $divisor++;
+        }
+
+        return $factors;
+    }
+}
